408ada719908a1ee8bfccd05e0e6f79616133c3e
Steve Nagel <stevencnagel@gmail.com>
2022-02-14 20:09:55 -0600

    Ch 14 - authentication added

--- src/ApplicationViews.js
+++ src/ApplicationViews.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Route, Routes } from "react-router-dom"
+import { Route, Routes, Navigate } from "react-router-dom"
 import { Home } from "./Home"
 import { AnimalList } from "./components/animal/AnimalList"
 import { LocationList } from "./components/location/LocationList"
@@ -11,26 +11,40 @@ import { AnimalForm } from "./components/animal/AnimalForm"
 import { CustomerForm } from "./components/customer/CustomerForm"
 import { EmployeeForm } from "./components/employee/EmployeeForm"
 import { LocationForm } from "./components/location/LocationForm"
+import { Login } from "./components/auth/Login"
+import { Register } from "./components/auth/Register"
 
-export const ApplicationViews = () => {
+export const ApplicationViews = ({ isAuthenticated, setIsAuthenticated }) => {
+
+    const PrivateRoute = ({ children }) => {
+        return isAuthenticated ? children : <Navigate to="/login" />;
+    }
+  
+    const setAuthUser = (user) => {
+      sessionStorage.setItem("kennel_customer", JSON.stringify(user))
+      setIsAuthenticated(sessionStorage.getItem("kennel_customer") !== null)
+    }
+
     return (
         <>
             <Routes>
+                <Route exact path="/login" element={<Login setAuthUser={setAuthUser} />} />
+                <Route exact path="/register" element={<Register />} />
                 {}
                 <Route path="/" element={<Home />} />
                 {}
-                <Route exact path="/animals" element={<AnimalList />} />
+                <Route exact path="/animals" element={<PrivateRoute><AnimalList /></PrivateRoute>} />
                 <Route path="/animals/:animalId" element={<AnimalDetail />} />
                 <Route path="/animals/create" element={<AnimalForm />} />
                 {}
-                <Route exact path="/locations" element={<LocationList />} />
+                <Route exact path="/locations" element={<PrivateRoute><LocationList /></PrivateRoute>} />
                 <Route path="/locations/:locationId" element={<LocationDetail />} />
                 <Route path="/locations/create" element={<LocationForm />} />
                 {}
-                <Route path="/customers" element={<CustomerList />} />
+                <Route path="/customers" element={<PrivateRoute><CustomerList /></PrivateRoute>} />
                 <Route path="/customers/create" element={<CustomerForm />} />
                 {}
-                <Route path="/employees" element={<EmployeeList />} />
+                <Route path="/employees" element={<PrivateRoute><EmployeeList /></PrivateRoute>} />
                 <Route path="/employees/create" element={<EmployeeForm />} />
             </Routes>
         </>
--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -1,58 +1,29 @@
-import React from "react"
-import { AnimalCard } from "./animal/AnimalCard"
-import { CustomerCard } from "./customer/CustomerCard"
-import { EmployeeCard } from "./employee/EmployeeCard"
-import { LocationCard } from "./location/LocationCard"
-import "./Kennel.css"
-import { PropsAndState } from "./PropsAndState"
+import React, {useState} from "react"
 import { NavBar } from "./nav/NavBar"
 import { ApplicationViews } from "../ApplicationViews"
 import "./Kennel.css"
 
-export const Kennel = () => (
-    <>
-        <NavBar />
-        <ApplicationViews />
-    </>
-)
+export const Kennel = () => {
+    const [isAuthenticated, setIsAuthenticated] = useState(sessionStorage.getItem("kennel_customer") !== null)
 
-// export const Kennel = () => (
-//     <>
-//         <h2>Nashville Kennels</h2>
-//         <small>Loving care when you're not there.</small>
-//         <address>
-//             <div>Visit Us at the Nashville North Location</div>
-//             <div>500 Puppy Way</div>
-//         </address>
+    const setAuthUser = (user) => {
+        sessionStorage.setItem("kennel_customer", JSON.stringify(user))
+        setIsAuthenticated(sessionStorage.getItem("kennel_customer") !== null)
+    }
 
-//         <PropsAndState yourName="Steve" />
-
-//         <h2>Animals</h2>
-//         <article className="animals">
-//             <AnimalCard />
-//             <AnimalCard />
-//             <AnimalCard />
-//         </article>
-
-//         <h2>Employees</h2>
-//         <article className="employees">
-//             <EmployeeCard />
-//             <EmployeeCard />
-//             <EmployeeCard />
-//         </article>
-
-//         <h2>Locations</h2>
-//         <article className="locations">
-//             <LocationCard />
-//             <LocationCard />
-//         </article>
-
-//         <h2>Customers</h2>
-//         <article className="customers">
-//             <CustomerCard/>
-//             <CustomerCard/>
-//             <CustomerCard/>
-//             <CustomerCard/>
-//         </article>
-//     </>
-// )
+    const clearUser = () => {
+        sessionStorage.clear();
+        setIsAuthenticated(sessionStorage.getItem("kennel_customer") !== null)
+      }
+    
+      return (
+        <>
+            <NavBar clearUser={clearUser} isAuthenticated={isAuthenticated}/>
+            <ApplicationViews 
+                setAuthUser={setAuthUser}
+                isAuthenticated={isAuthenticated}
+                setIsAuthenticated={setIsAuthenticated}
+            />
+        </>
+    )
+}
